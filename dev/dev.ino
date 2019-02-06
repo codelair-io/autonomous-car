b9e40df8d0d50508838c6efd445d4953cbc62d5a
Hassan Nazar <hnazar@redbridge.se>
2019-02-06 20:17:49 +0100

    - reglating power between 11-13 and 2-3

--- dev/dev.ino
+++ dev/dev.ino
@@ -1,9 +1,56 @@
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Blink
+*/
+
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
-
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(11, OUTPUT);
+  
+  Serial.begin(9600);
 }
-
+int i = 0;
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  delay(2000);
+  if(i++%100 == 0) {
+    Serial.println("Fuck the world!");
+  }
+  if(i%2 == 0) {
+    digitalWrite(13, HIGH);
+    digitalWrite(11, LOW);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    Serial.println("Bleh?");
+  } else {
+    digitalWrite(13, LOW);
+    digitalWrite(11, HIGH);
+    digitalWrite(2, LOW);
+    digitalWrite(3, HIGH);
+    Serial.println("WAAAAGH!!!");
+  }  
 }
